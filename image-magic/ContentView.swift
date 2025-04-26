e04bc9ba692811f364dbd7671722f353a66fdf27
Mahin Ibrahim <mahin8514@gmail.com>
2025-04-26 12:56:34 +0400

    Initial Commit

---
+++ image-magic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  image-magic
+//
+//  Created by mahi  on 26/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ image-magic/image_magicApp.swift
@@ -0,0 +1,17 @@
+//
+//  image_magicApp.swift
+//  image-magic
+//
+//  Created by mahi  on 26/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct image_magicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
